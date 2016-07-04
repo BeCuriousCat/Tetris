4bffee1932be645e521e556bf0b8f1fcf34ed5a6
BeCuriousCat <1454787565@qq.com>
2016-07-04 20:16:20 +0800

    合并提供下载master

--- src/ui/LayerLevel.java
+++ src/ui/LayerLevel.java
@@ -14,8 +14,14 @@ public class LayerLevel extends Layer {
 	 * 数字图片 230 36
 	 */
 	private static Image IMG_NUM = new ImageIcon("graphics/string/num.png").getImage();
-	private static final int  IMG_NUM_W = 26;
-	private static final int  IMG_NUM_H = 36;
+	/**
+	 * 数字切片的宽度
+	 */
+	private static final int  IMG_NUM_W = IMG_NUM.getWidth(null) / 10;
+	/**
+	 * 数字切片的高度
+	 */
+	private static final int  IMG_NUM_H = IMG_NUM.getHeight(null);
 	
 	
 	public LayerLevel(int x, int y, int w, int h) {
@@ -26,21 +32,31 @@ public class LayerLevel extends Layer {
 		//窗口标题 
 		g.drawImage(IMG_LEVEL, this.x+PADDING, this.y+PADDING, null);
 		//显示等级
-		drawNumber(48,64,1, g);
+		
+		drawNumberLeftPad(64,32,1, 3,g);
 	}
 	/**
 	 * 显示数字
-	 * @param g
+	 * @param x 左上角x坐标
+	 * @param y 左上角y坐标
+	 * @param num 要显示的数字
+	 * @param maxBit 数字位数 
+	 * @param g	画笔对象
 	 */
-	public void drawNumber(int x,int y,int num,Graphics g){
-		//把数组number中的每一位取出来
+	public void drawNumberLeftPad(int x,int y,int num,int maxBit,Graphics g){
+		//把数组num中的每一位取出来
 		String strNum = Integer.toString(num);
-		for (int i = 0; i < strNum.length(); i++) {
+		//循环绘制数字右对齐
+		for (int i = 0; i < maxBit; i++) {
+			//判断是否满足绘制条件
+			if( i >= maxBit - strNum.length()) return ;
+			//把数字num中每一位取出
 			int bit = strNum.charAt(i) - '0';
+			//绘制数字
 			g.drawImage(IMG_NUM, 
 					this.x + x + IMG_NUM_W * i ,this.y + y, 
 					this.x + x + IMG_NUM_W * ( i + 1 ), this.y + y + IMG_NUM_H, 
-					num * IMG_NUM_W, 0,
+					bit * IMG_NUM_W, 0,
 					(bit+1)*IMG_NUM_W, IMG_NUM_H, null);
 		}
 	}
